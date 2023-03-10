3c010759a63675f742dd01139894b9fc31b1193b
dangminhngo <dangminhngo.dev@gmail.com>
2023-03-10 17:09:43 +0700

    fix(styles): stretch input width in Subscribe component

--- src/app/Subscribe.tsx
+++ src/app/Subscribe.tsx
@@ -2,12 +2,12 @@ export default function Subscribe() {
   return (
     <div className="flex flex-col items-center gap-6">
       <h1 className="mb-2 text-2xl font-bold">Subscribe to my Newsletter</h1>
-      <form className="relative w-[512px] rounded-lg bg-gray-700">
+      <form className="relative w-[512px] flex p-2 items-center rounded-lg bg-gray-700">
         <input
-          className="rounded-lg bg-transparent px-6 py-4 outline-none placeholder:text-gray-500"
+          className="px-2 w-full rounded-lg bg-transparent outline-none placeholder:text-gray-500"
           placeholder="Enter your email address"
         />
-        <button className="absolute top-1/2 right-0 -translate-x-2 -translate-y-1/2 rounded-lg bg-gray-900 px-6 py-2 font-semibold">
+        <button className="rounded-lg bg-gray-900 px-6 py-2 font-semibold">
           Submit
         </button>
       </form>
