7035dc831b44f4c5205aa9c6725039ebef4ab4d0
dangminhngo <dangminhngo.dev@gmail.com>
2023-03-10 23:44:41 +0700

    fix(styles): reduce mini card size

--- src/components/ArticleMiniCard.tsx
+++ src/components/ArticleMiniCard.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image'
 import { Frontmatter } from '@/types'
 import HashTagGrid from './HashTagGrid'
+import Link from 'next/link'
 
 export interface ArticleMiniCardProps {
   frontmatter: Frontmatter
 }
 
 export default function ArticleMiniCard({
-  frontmatter: { id, title, tags, date, readingTime, cover },
+  frontmatter: { id, title, slug, tags, date, readingTime, cover },
 }: ArticleMiniCardProps) {
   return (
     <div className="flex items-center gap-4" data-id={id}>
-      <div className="relative h-[160px] min-w-[120px] overflow-hidden rounded-lg">
+      <div className="relative h-[120px] min-w-[90px] overflow-hidden rounded-lg">
         <Image src={cover} alt={title} fill={true} />
       </div>
-      <div className="space-y-3 text-sm text-gray-300">
+      <div className="flex flex-col items-stretch gap-2 text-sm text-gray-300">
         <HashTagGrid tags={tags} />
-        <h3 className="text-lg font-bold leading-snug text-gray-200">{title}</h3>
+        <Link href={`/${slug}`} className="text-base font-bold leading-snug text-gray-200">
+          {title}
+        </Link>
         <div className="flex items-center gap-2">
           <span>{date}</span>
           <span className="block h-2 w-2 rounded-full bg-red-400">&nbsp;</span>
