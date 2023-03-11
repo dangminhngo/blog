c88c4833500260cf09d737d0b43b79969e9913e3
dangminhngo <dangminhngo.dev@gmail.com>
2023-03-11 09:25:23 +0700

    fix(styles): fix underline of hash tags

--- src/components/HashTag.tsx
+++ src/components/HashTag.tsx
@@ -10,7 +10,7 @@ export default function HashTag({ tag }: HashTagProps) {
     <Link href={`/tags/${tag.name}`} className="group">
       <p
         className="inline bg-[length:0_1px] bg-[0_100%] bg-no-repeat duration-300 group-hover:bg-[length:100%_1px]"
-        style={{ backgroundImage: `linear-gradient(${tag.color})` }}
+        style={{ backgroundImage: `linear-gradient(${tag.color}, ${tag.color})` }}
       >
         <span style={{ color: tag.color }}>#</span>
         <span>{tag.name}</span>
